26ee23dbc13dec40da9e557ea11b4bc32a68d43f
vamshi <yuvnith.vamshi@gmail.com>
2018-03-28 10:41:42 +0530

    new commit

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LINQ
 {
     public delegate bool Del(Student stud);
+
+    public delegate bool Del2(Student stud, int age);
     class Program
     {
         static void Main(string[] args)
@@ -16,12 +19,25 @@ namespace LINQ
             Student[] studentArray = {
                 new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
                 new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
-                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
-                new Student() { StudentID = 4, StudentName = "Ram" , Age = 21 } ,
-                new Student() { StudentID = 5, StudentName = "Ron" , Age = 21 } ,
+                new Student() { StudentID = 3, StudentName = "Ram",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 23 } ,
+                new Student() { StudentID = 5, StudentName = "Ram" , Age = 21 } ,
                 new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 } ,
                 new Student() { StudentID = 7, StudentName = "Rob",Age = 19  } ,
             };
+
+            Student2[] studentArray2 = {
+                new Student2() { StudentID = 1, Marks = 12 } ,
+                new Student2() { StudentID = 2,  Marks = 25 } ,
+                new Student2() { StudentID = 3,Marks = 6 } ,
+                new Student2() { StudentID = 4,Marks = 20 } ,
+                new Student2() { StudentID = 5,  Marks = 21 } ,
+                new Student2() { StudentID = 6,  Marks = 7 } ,
+                new Student2() { StudentID = 7, Marks = 19  } ,
+            };
+
+            var aList = new ArrayList(){1,"a",'d',233,'s',"asdsa",'a',"fdsa"};
+            
 
             string[] abc = { "abcd", "efgh", "easdfasd", "sa" };
 
@@ -62,12 +78,87 @@ namespace LINQ
             var res9 = from s7 in studentArray
                 where s7.StudentID == 1
                 select s7.StudentName;
-                       
 
-            foreach (var re in res9)
+            var res10 = from v2 in studentArray
+                where v2.StudentName == "John"
+                select v2.Age;
+
+
+            Del2 del2 = (Student s, int age) => s.Age < age; 
+            //Console.WriteLine(del2(studentArray[0],20));
+
+            var res11 = studentArray.Where((s ) => s.Age == 18);
+
+            Func<Student, bool> func1 = s => s.Age > 18;
+
+            var res12 = studentArray.Where(func1);
+
+
+            var res13 = from s in studentArray
+                where func1(s)
+                select s;
+
+
+            var res14 = studentArray.Where((s, i) => i == 1 );
+
+
+            var res15 = studentArray.Where(s => s.Age > 18).Where(a => a.Age < 25);
+
+            var res16 = from s in aList.OfType<int>()
+                        select s;
+
+            var res17 = from s in studentArray
+                orderby s.StudentName descending 
+                select s;
+
+            var res18 = studentArray.OrderBy(s => s.Age);
+            var res19 = studentArray.OrderByDescending(s => s.Age).Where(s=>s.Age==21);
+
+            var res20 = from s in studentArray
+                orderby s.Age
+                where s.Age == 21
+                select new { s.Age, s.StudentName };
+
+            var res21 = studentArray.OrderBy(s => s.StudentName).ThenBy(s => s.Age);
+
+            var res22 = from s9 in studentArray
+                group s9 by s9.Age;
+
+            var res23 = studentArray.GroupBy(s => s.Age);
+            
+            //foreach (var re in res23)
+            //{
+            //    Console.WriteLine(re.Key);
+            //    foreach (var student in re)
+            //    {
+            //        Console.WriteLine(student.StudentName+" "+student.Age);
+            //    }
+            //}
+
+
+
+            var res24 = studentArray.Join(studentArray2, student => student.StudentID, student2 => student2.StudentID,
+                (student, student2) => new {student.StudentName, student2.Marks});
+
+
+
+            var res25 = from s in studentArray
+                join s2 in studentArray2 on s.StudentID equals s2.StudentID
+                select new
+                {
+                    s.StudentName,
+                    s2.Marks
+                };
+
+            foreach (var re in res25)
             {
-                Console.WriteLine(re);
+                Console.WriteLine(re.StudentName+" "+re.Marks);
+              
             }
+
+
+
+
 
             Console.ReadKey();
         }
---
+++ Student2 .cs
@@ -0,0 +1,8 @@
+﻿namespace LINQ
+{
+    public class Student2 
+    {
+        public int StudentID { get; set; }
+        public int Marks { get; set; }
+    }
+}
